7b346d9ea446568b0a2f24837d5c0c6464428c6b
littlelucidlynx <dmitry.man5on@gmail.com>
2025-05-05 11:06:13 +0300

    update project files

--- init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build -t nginx-static-app .
-yc container registry configure-docker
-docker tag nginx-static-app cr.yandex/crpueiqeqavuee81fvro/nginx-static-app:latest
-docker push cr.yandex/crpueiqeqavuee81fvro/nginx-static-app:latest
